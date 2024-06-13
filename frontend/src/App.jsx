2ea58d37124de1bff50baa33e76a92a7af98cd8a
Kusha008 <kushagra.anand2004@gmail.com>
2024-06-13 15:35:45 +0530

    fixed bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { LoginPage, RegisterPage,VerifyPage } from './Routes.js';
+import { LoginPage, RegisterPage,VerifyPage,HomePage } from './Routes.js';
 
 
 import './App.css';
@@ -13,6 +13,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage/>} />
         <Route path='/verify' element={<VerifyPage/>} />
+        <Route path='/' element={<HomePage />} />
       </Routes>
     </Router>
   );
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,11 @@
 import LoginPage from "../src/pages/Login.jsx"
 import RegisterPage from "../src/pages/Register.jsx"
 import VerifyPage from "../src/pages/Verify.jsx"
+import HomePage from "../src/pages/Home.jsx"
 
 export {
     LoginPage,
     RegisterPage,
-    VerifyPage
+    VerifyPage,
+    HomePage
 }
